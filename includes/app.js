18a802b00c7ff7f479fea9418793a5c349ce4c85
Yves Courtois <Courtois1337@gmail.com>
2014-11-11 21:14:28 -0600

    made page views work w/ the controller

--- includes/app.js
+++ includes/app.js
@@ -1,4 +1,16 @@
-angular.module('portfolio',[])
+angular.module('portfolio',['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/projects', {
+        templateUrl: '/includes/views/projects.html',
+        controller: 'dataListCtrl'
+    }).
+    when('/', {
+        templateUrl: '/includes/views/info.html'
+    }).
+    otherwise({
+        redirectTo: ''
+    });
+}])
 .controller("dataListCtrl", function($scope, $http) {
     $scope.data = {};
 
