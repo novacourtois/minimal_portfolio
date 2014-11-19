8575df08eb78f5e3053e145f4b8fb664dadd2215
Yves Courtois <Courtois1337@gmail.com>
2014-11-18 20:48:49 -0600

    controller and styling for the new changes

--- includes/app.js
+++ includes/app.js
@@ -1,8 +1,12 @@
 angular.module('portfolio',['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/projects', {
-        templateUrl: '/includes/views/projects.html',
+    $routeProvider.when('/work', {
+        templateUrl: '/includes/views/work.html',
         controller: 'dataListCtrl'
+    }).
+    when('/projects', {
+        templateUrl: '/includes/views/projects.html',
+        controller: 'projectListCtrl'
     }).
     when('/', {
         templateUrl: '/includes/views/info.html'
@@ -11,12 +15,30 @@ angular.module('portfolio',['ngRoute'])
         redirectTo: '/'
     });
 }])
+.controller("projectListCtrl", function($scope, $http) {
+    $scope.data = {};
+
+    $http.get('http://novacourtois.com/includes/projects.json').
+        success(function(data) {
+            $scope.data = data;
+        });
+})
 .controller("dataListCtrl", function($scope, $http) {
     $scope.data = {};
 
-    $http.get('http://novacourtois.com/data.json').
+    $http.get('http://novacourtois.com/includes/projects.json').
         success(function(data) {
-            $scope.data = data;
+            $scope.data.projects = data.items;
+        });
+
+    $http.get('http://novacourtois.com/includes/other.json').
+        success(function(data) {
+            $scope.data.other = data.items;
+        });
+
+    $http.get('http://novacourtois.com/includes/websites.json').
+        success(function(data) {
+            $scope.data.websites = data.items;
         });
 })
 .filter('unsafe', function($sce) {
